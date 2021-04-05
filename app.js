dc7e4930410757afc0329c5f1a526cfa2cb1570d
Akhmadkhuja <shamshiddinov00@mail.ru>
2021-04-06 01:03:33 +0500

    minor update

--- app.js
+++ app.js
@@ -1,11 +1,66 @@
-const express = require(express)
+
+
+const express = require('express')
 const app = express()
+
+const fs = require('fs')
 const PORT = 8000
 
 app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended: false }))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    fs.readFile('./data/todos.json', (err, data) => {
+        if (err) throw err
+
+        const todos = JSON.parse(data)
+
+        res.render('home', { todos: todos})
+    })
+})
+
+app.post('/add' , (req, res) => {
+    const formData = req.body
+
+    if (formData.todo.trim() == '') {
+
+        fs.readFile('./data/todos.json', (er, data) => {
+            if (err) throw err
+            const todos = JSON.parse(data)
+
+            res.render('home', {error: true, todos: todos })
+        })
+        fs.readFile('./data/todos.json', (er, data) => {
+                    if (err) throw err
+                    const todos = JSON.parse(data)
+
+                    res.render('home', {success: true, todos: todos })
+                })
+    } else {
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todo = {
+                id: id(),
+                description: formData.todo,
+                done: false
+            }
+
+            todos.push(todo)
+
+            fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+                if (err) throw err
+
+                fs.readFile('./data/todos.json', (er, data) => {
+                    if (err) throw err
+                    const todos = JSON.parse(data)
+
+                    res.render('home', {success: true, todos: todos })
+                })
+            })
+        })
+    }
 })
 
 app.listen(PORT, (err) => {
@@ -13,3 +68,8 @@ app.listen(PORT, (err) => {
 
     console.log(`App is running on port ${PORT}`)
 })
+
+
+function id() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
